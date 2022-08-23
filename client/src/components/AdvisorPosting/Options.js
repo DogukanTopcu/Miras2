05b8b57602efa6c8eb97f58d9538e33cdd74abe3
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-08-23 23:49:57 +0300

    advisor posting options panel frontend

--- client/src/components/AdvisorPosting/Options.js
+++ client/src/components/AdvisorPosting/Options.js
@@ -12,6 +12,17 @@ const Options = () => {
                 <div className='normal-star'><StarIcon /></div>
             </div>
             <h2>Uzman İlanı</h2>
+            <div className='features'>
+                <p>Özellikler:</p>
+                <ul>
+                    <li>30 gün boyunca ilanınız yayınlanır.</li>
+                    <li>Vereceğiniz ilk ders bedava olmak zorundadır.</li>
+                    <li>İlan süresi dahilinde ilanınızı VIP üyeliğine ya da haftalık VIP üyeliğine yükseltilebilir.</li>
+                    <li>İlanınızı istediğiniz zaman düzenleyebilirsiniz</li>
+                </ul>
+            </div>
+
+            <button className='card-button'>Uzman İlanı</button>
         </div>
         <div className='option-card options-VIP'>
             <div className='option-card-top'>
@@ -19,6 +30,18 @@ const Options = () => {
                 <div className='vip-star'><StarIcon /></div>
             </div>
             <h2>VIP Uzman İlanı</h2>
+            <div className='features'>
+                <p>Özellikler:</p>
+                <ul>
+                    <li>30 gün boyunca ilanınız yayınlanır.</li>
+                    <li>Vereceğiniz ilk ders bedava olmak zorunda değildir.</li>
+                    <li>İlan süresi dahilinde ilanınız ana sayfada en üstte gözükecektir.</li>
+                    <li>İlanınızı istediğiniz zaman düzenleyebilirsiniz.</li>
+                    <li>İlanınız ana sayfada diğer ilanlara göre daha büyük gözükecektir.</li>
+                </ul>
+            </div>
+
+            <button className='card-button'>Uzman İlanı</button>
         </div>
         <div className='option-card options-weeklyVIP'>
             <div className='option-card-top'>
@@ -26,6 +49,17 @@ const Options = () => {
                 <div className='week-star'><StarIcon /></div>
             </div>
             <h2>Haftanın VIP İlanı</h2>
+            <div className='features'>
+                <p>Özellikler:</p>
+                <ul>
+                    <li>7 gün boyunca ilanınız yayınlanır.</li>
+                    <li>Vereceğiniz ilk ders bedava olmak zorunda değildir.</li>
+                    <li>Haftalık VIP süresi dolduğunda ilanınız, normal ilan süresindeki kalan gün kadar devam eder.</li>
+                    <li>İlanınızı istediğiniz zaman düzenleyebilirsiniz</li>
+                </ul>
+            </div>
+
+            <button className='card-button'>Uzman İlanı</button>
         </div>
     </div>
   )
